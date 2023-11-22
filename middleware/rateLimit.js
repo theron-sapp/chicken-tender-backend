ab50387fabc527c29f1db2141e869c71c84fe8e3
theron-sapp <150622627+theron-sapp@users.noreply.github.com>
2023-11-21 21:07:32 -0500

    Update rateLimit.js

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 
 export const createAccountLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour window
-  max: 10, // limit each IP to 5 create account requests per windowMs
+  max: 10, // limit each IP to 10 session creates
   message:
     "Too many accounts created from this IP, please try again after an hour",
 });
