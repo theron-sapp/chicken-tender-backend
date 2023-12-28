8c37952e02993b41bd88568bf21598e6c099846e
Theron Sapp <theronsapp@gmail.com>
2023-12-28 15:08:49 -0500

    Creating a log-message endpoint so the client can log the errors to the server for debugging.

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -28,5 +28,10 @@ router.get("/:code/results", getSessionResults);
 router.get("/:code/details", getSession);
 router.put("/:code/user/:username/donevoting", updateUserVotingStatus);
 router.delete("/:code/:username/delete", deleteSession);
+router.post("/log-message", (req, res) => {
+  const message = req.body.message;
+  console.log(message); // Log the message to the console
+  res.status(200).send("Message received and logged.");
+});
 
 export default router;
